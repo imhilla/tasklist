227efb6f76cee7a6f9ecc10fdbb1fb986f1b9c92
imhilla <hillaryodhiambo282@gmail.com>
2023-08-23 11:00:15 +0300

    first break

--- tasklist/resources/views/index.blade.php
+++ tasklist/resources/views/index.blade.php
@@ -1,6 +1,12 @@
-Hello
-I am a blade template
-
-@isset($name)
-    <div>The name is {{ $name }}</div>
-@endisset
+<h1>The list of tasks</h1>
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                {{ $task->title }}
+            </a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+</div>
