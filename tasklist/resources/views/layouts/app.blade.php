83de9fa1c587f497d2571df4cc8c1f5f2695bb30
imhilla <hillaryodhiambo282@gmail.com>
2023-08-24 17:49:08 +0300

    updates

--- tasklist/resources/views/layouts/app.blade.php
+++ tasklist/resources/views/layouts/app.blade.php
@@ -7,6 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 10 Task list app</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <style type="text/tailwindcss">
+        .btn {
+            @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+        }
+
+        .link {
+            @apply font-medium text-gray-700 underline decoration-pink-500
+        }
+    </style>
     @yield('styles')
 </head>
 
